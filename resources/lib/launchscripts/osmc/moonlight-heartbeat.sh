4bdd41d7b468db8a435918108099348b54508346
meveric <schlurf82@googlemail.com>
2017-03-04 14:57:36 +0100

    Adapt changes for upstream

--- resources/lib/launchscripts/osmc/moonlight-heartbeat.sh
+++ resources/lib/launchscripts/osmc/moonlight-heartbeat.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+POST_SCRIPT=$1
+
 killall -STOP kodi.bin
 
 sleep 10
@@ -7,6 +9,10 @@ sleep 10
 while [ true ]; do
         status="$(pidof moonlight | wc -w)"
         if [ ${status} -ne 1 ]; then
+            if [ ${POST_SCRIPT} != "" ]; then
+                ${POST_SCRIPT}
+            fi
+
             killall -CONT kodi.bin
             exit
         else
